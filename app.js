8c04352856ee6c8b8fd2a180e93a11796000c189
Ryan Spink <ryanspinktech@gmail.com>
2018-03-29 20:32:58 -0600

    predicting adult vs child

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const brain = require("brain.js")
 
 const network = new brain.NeuralNetwork()
 
-network.train()
+network.train([
+  {input:{height: 60, weight: 150}, output:{adult: 1}},
+  {input:{height: 70, weight: 160}, output:{adult: 1}},
+  {input:{height: 30, weight: 60}, output:{adult: 0}}
+])
 
-const result = network.run()
+const result = network.run({height: 70, weight: 170})
+
+console.log(result);
